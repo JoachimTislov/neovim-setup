d4ea25bd4f16fcb3d51a7c7c32e86dec9fccc7a1
JoachimTislov <joachim.tislov@gmail.com>
2025-06-25 01:02:53 +0200

    update

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,15 @@
+require('lazy').setup {
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  require 'kickstart.plugins.telescope',
+  require 'kickstart.plugins.lazy-dev',
+  require 'kickstart.plugins.lspconfig',
+  require 'kickstart.plugins.blink-cmp',
+  require 'kickstart.plugins.tokyonight',
+  require 'kickstart.plugins.mini',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent-line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns',
+}
---
+++ lua/options.lua
@@ -0,0 +1,69 @@
+-- Formatting
+vim.opt.tabstop = 2 -- Number of visual spaces per tab
+vim.opt.softtabstop = 2 -- Spaces inserted when pressing Tab
+vim.opt.shiftwidth = 2 -- Spaces used for auto-indent
+vim.opt.expandtab = true -- Use spaces instead of tab characters
+
+-- Make line numbers default
+vim.o.number = true
+-- You can also add relative line numbers, to help with jumping.
+-- Experiment for yourself to see if you like it!
+-- vim.o.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+-- vim -r *FileName*
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Default 1000
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+
