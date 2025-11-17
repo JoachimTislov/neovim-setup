b75a273b912a4246b579729cb23f18255ee7456e
JoachimTislov <joachim.tislov@gmail.com>
2025-11-17 22:55:14 +0100

    add more configurations, mostly migrating from linux

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.g.have_nerd_font = false
 vim.o.wrap = true
 vim.o.winborder = 'rounded'
 vim.swapfile = false
+vim.o.splitright = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = 'yes'
@@ -17,21 +18,54 @@ vim.o.scrolloff = 15
 vim.o.list = true
 vim.opt.listchars = { trail = '·' }
 
-local github = 'https://github.com/'
+-- TODO: Add snippet support
 vim.pack.add({
-  { src = github .. 'stevearc/oil.nvim' },
-  { src = github .. 'echasnovski/mini.pick' },
-  { src = github .. 'neovim/nvim-lspconfig' },
-  { src = github .. 'williamboman/mason.nvim' },
-  { src = github .. 'WhoIsSethDaniel/mason-tool-installer.nvim' },
-  { src = github .. 'folke/which-key.nvim' },
-
-  -- Themes
-  { src = github .. 'rose-pine/neovim' },
-  { src = github .. 'vague2k/vague.nvim' },
+  'https://github.com/nvim-lua/plenary.nvim', -- dependency of many plugins. Storage of complete lua functions
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/echasnovski/mini.pick',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/williamboman/mason.nvim',
+  'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
+  'https://github.com/folke/which-key.nvim',
+  'https://github.com/CopilotC-Nvim/CopilotChat.nvim',
+  'https://github.com/rose-pine/neovim',
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  'https://github.com/windwp/nvim-autopairs',
+  'https://github.com/windwp/nvim-ts-autotag',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/catgoose/nvim-colorizer.lua',
+  'https://github.com/artemave/workspace-diagnostics.nvim',
+  'https://github.com/github/copilot.vim',
+  'https://github.com/lewis6991/gitsigns.nvim',
 })
+require 'colorizer'.setup()
+require 'workspace-diagnostics'.setup()
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { 'svelte', 'css', 'html', 'typescript', 'java' },
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  }
+}
+require 'nvim-autopairs'.setup()
+require 'nvim-ts-autotag'.setup()
+require 'rose-pine'.setup {
+  styles = {
+    bold = true,
+    italic = false,
+    transparency = true
+  }
+}
+
 vim.cmd.colorscheme('rose-pine')
 vim.cmd(':hi statusline guibg=NONE')
+
+require 'render-markdown'.setup {
+  file_types = { 'copilot-chat' }
+}
 
 require('mason').setup()
 require('mason-tool-installer').setup {
@@ -60,6 +94,14 @@ local function nvmap(...)
   k({ 'n', 'v', 'x' }, ...)
 end
 
+local function vmap(...)
+  k('v', ...)
+end
+
+local function imap(...)
+  k('i', ...)
+end
+
 -- Sync with system clipboard
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
@@ -69,23 +111,147 @@ require 'mini.pick'.setup()
 require 'oil'.setup()
 require('which-key').setup {
   delay = 500,
+  icons = { mappings = vim.g.have_nerd_font },
   win = {
     no_overlap = false,
   },
+  spec = {
+    { '<leader>q', group = '[Q]uit' },
+    { '<leader>p', group = '[P]ick' },
+    { '<leader>o', group = '[O]pen' },
+    { '<leader>c', group = '[C]opilot' },
+    { '<leader>g', group = '[G]it Hunk', mode = { 'n', 'v' } }
+  }
 }
 
-nmap('<leader>f', '<cmd>Pick files<cr>', { desc = 'Pick files' })
-nmap('<leader>h', '<cmd>Pick help<cr>', { desc = 'Pick help' })
-nmap('<s-h>', '<cmd>Oil<cr>', { desc = 'Oil' })
-nmap('<leader>lf', vim.lsp.buf.format, { desc = 'Format file' })
-nmap('<leader>w', '<cmd>w<cr>', { desc = 'Write to file' })
-nmap('<leader>e', '<cmd>q<cr>', { desc = 'Quit file' })
-nmap('<leader>s', ':source<cr>')
-nmap('<leader>os', '<cmd>split<cr>')
-nmap('<leader>ov', '<cmd>vsplit<cr>')
+require 'CopilotChat'.setup {
+  model = 'claude-sonnet-4',
+  temperature = 0, -- Lower = focused, higher = creative
+  sticky = {
+    '#buffer',
+  },
+  highlight_headers = false,
+  auto_fold = true,
+  auto_insert_mode = false,
+  mappings = {
+    reset = false,
+  },
+  headers = {
+    user = ' ',
+    assistant = ' ',
+    tool = '󰊳 ',
+  },
+  separator = '━━',
+  window = {
+    layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace'
+    -- width = 0.5, -- 50% of screen width
+    border = 'solid',    -- 'single', 'double', 'rounded', 'solid'
+  },
+}
 
--- Search with backspace
-nvmap('<BS>', '/')
+-- Remap <Tab> to <S-Tab> for copilot accept to avoid conflict with other plugins
+imap('<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+imap('<C-A-j>', 'copilot#Next()', { expr = true, silent = true, script = true })
+
+require 'gitsigns'.setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  on_attach = function(bufnr)
+    local gitsigns = require 'gitsigns'
+
+    local function map(mode, l, r, opts)
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts or {})
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { ']c', bang = true }
+      else
+        --- @diagnostic disable-next-line: param-type-mismatch
+        gitsigns.nav_hunk 'next'
+      end
+    end, { desc = 'Jump to next git [c]hange' })
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { '[c', bang = true }
+      else
+        --- @diagnostic disable-next-line: param-type-mismatch
+        gitsigns.nav_hunk 'prev'
+      end
+    end, { desc = 'Jump to previous git [c]hange' })
+
+    -- Actions
+    -- visual mode
+    vmap('<leader>gs', function()
+      gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end, { desc = 'git [s]tage hunk' })
+    vmap('<leader>gr', function()
+      gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+    end, { desc = 'git [r]eset hunk' })
+    -- normal mode
+    nmap('<leader>gs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+    nmap('<leader>gr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+    nmap('<leader>gS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+    nmap('<leader>gu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
+    nmap('<leader>gR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+    nmap('<leader>gp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+    nmap('<leader>gb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+    nmap('<leader>gd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+    nmap('<leader>gD', function()
+      --- @diagnostic disable-next-line: param-type-mismatch
+      gitsigns.diffthis '@'
+    end, { desc = 'git [D]iff against last commit' })
+    -- Toggles
+    nmap('<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+    nmap('<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+  end,
+}
+
+
+-- [Q]uit
+nmap('<leader>qa', '<cmd>qa<cr>', { desc = '[Q]uit [A]ll' })
+nmap('<leader>qt', '<cmd>tabc<cr>', { desc = '[Q]uit [T]ab' })
+nmap('<leader>qb', '<cmd>bd<cr>', { desc = '[Q]uit [B]uffer' })
+nmap('<leader>qf', '<cmd>q!<cr>', { desc = '[Q]uit [F]orce' })
+
+-- [O]pen
+nmap('<leader>om', '<cmd>messages<cr>', { desc = '[O]pen [M]essages' })
+nmap('<leader>oq', '<cmd>copen<cr>', { desc = '[O]pen [Q]uicklist' })
+nmap('<leader>om', '<cmd>Mason<cr>', { desc = '[O]pen [M]ason' })
+nmap('<leader>os', '<cmd>split<cr>', { desc = '[O]pen [S]plit' })
+nmap('<leader>ov', '<cmd>vsplit<cr>', { desc = '[O]pen [V]ertical split' })
+
+-- [C]opilot
+nmap('<leader>cp', '<cmd>CopilotChatPrompts<cr>', { desc = 'View/select prompt templates' })
+nmap('<leader>ct', '<cmd>CopilotChatToggle<cr>', { desc = 'Toggle chat window' })
+nmap('<leader>cs', '<cmd>CopilotChatStop<cr>', { desc = 'Stop current output' })
+nmap('<leader>cm', '<cmd>CopilotChatModels<cr>', { desc = 'View/select available models' })
+nmap('<leader>cw', ':CopilotChatSave ', { desc = 'Save chat' })
+nmap('<leader>cl', ':CopilotChatLoad ', { desc = 'Load chat' })
+
+-- [P]ick
+nmap('<leader>pf', '<cmd>Pick files<cr>', { desc = '[P]ick [F]iles' })
+nmap('<leader>ph', '<cmd>Pick help<cr>', { desc = '[P]ick [H]elp' })
+nmap('<leader>pb', '<cmd>Pick buffers<cr>', { desc = '[P]ick [B]uffers' })
+
+-- Actions
+nmap('<s-h>', '<cmd>Oil<cr>', { desc = 'Oil (File browser)' })
+nmap('<leader>lf', vim.lsp.buf.format, { desc = 'Format file' })
+nmap('<leader>e', '<cmd>q<cr>', { desc = '[E]xit' })
+nmap('<leader>w', '<cmd>w<cr>', { desc = 'Write to file' })
+nmap('<leader>s', ':source<cr>')
+
+-- Copilot
+nmap('<leader>ce', '<cmd>Copilot enable<cr>', { desc = 'Enable' })
+nmap('<leader>cd', '<cmd>Copilot disable<cr>', { desc = 'Disable' })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 nmap('<Esc>', '<cmd>nohlsearch<cr>')
@@ -94,7 +260,7 @@ nmap('<Esc>', '<cmd>nohlsearch<cr>')
 nvmap('<C-j>', '<C-W>j')
 nvmap('<C-k>', '<C-W>k')
 nvmap('<C-l>', '<C-W>l')
-nvmap('<C-h>', '<C-W>h')
+nvmap('<BS>', '<C-W>h') -- Attempt fix by something like this: https://github.com/neovim/neovim/issues/2048#issuecomment-78045837
 
 -- Center editor view with zz
 nvmap('<C-d>', '<C-d>zz')
@@ -109,6 +275,42 @@ nvmap('<M-h>', '<cmd>cfirst<cr>')
 nvmap('<M-l>', '<cmd>clast<cr>')
 
 -- Quality of life configurations
+
+vim.diagnostic.config {
+  severity_sort = true,
+  virtual_lines = false,
+  float = {
+    border = 'rounded',
+    source = 'if_many',
+  },
+  signs = vim.g.have_nerd_font and {
+    spacing = 2,
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+}
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = 'copilot-*',
+  callback = function()
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
 local snoremap = { noremap = true, silent = true }
 
 -- Disable space
@@ -142,8 +344,8 @@ end, snoremap)
 
 -- Override the handler to filter out unwanted diagnostics
 local lsp_symbols_filters = {
-  ['lua_ls'] = {
-    ['global'] = {
+  lua_ls = {
+    global = {
       'vim',
       'error',
       'string',
@@ -152,7 +354,7 @@ local lsp_symbols_filters = {
       'os_uname',
       'os',
     },
-    ['field'] = {
+    field = {
       'fs_stat',
     },
   },
